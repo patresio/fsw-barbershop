55712e57a3e8fbeaff085cae9ec46dbfb3bebc4c
Ricardo Pereira <patresio@gmail.com>
2024-01-30 15:14:36 -0300

    feat: initial add booking component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,3 +1,4 @@
+import BookingItem from "../_components/booking-item";
 import Header from "../_components/header";
 import Search from "./_components/search";
 import Welcome from "./_components/welcome";
@@ -11,6 +12,10 @@ export default function Home() {
       </div>
       <div className="px-5 mt-6">
         <Search />
+      </div>
+      <div className="px-5 mt-6">
+        <h2 className="text-xs uppercase text-gray-400 font-bold mb-3">Agendamentos</h2>
+        <BookingItem />
       </div>
     </div>
   );
