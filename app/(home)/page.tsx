03e1e43ca949eb3ee2e74cfb4d35142c33badcbd
Ricardo Pereira <patresio@gmail.com>
2024-01-31 07:42:15 -0300

    feat: add initial footer component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -32,6 +32,14 @@ export default async function Home() {
             ))}
         </div>
       </div>
+      <div className="px-5 mt-6 mb-[4.5rem]">
+        <h2 className="px-5 text-xs uppercase text-gray-400 font-bold mb-3">Populares</h2>
+        <div className="flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop:any) => (
+              <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+            ))}
+        </div>
+      </div>
     </div>
   );
 }
